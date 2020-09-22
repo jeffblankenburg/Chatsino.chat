d7021b26454b4fae42126fc8ac7b6e7954dbf0fe
Jeff Blankenburg <github@jeffblankenburg.com>
2020-09-22 15:27:03 -0400

    Added leaderboard page.

---
+++ leaderboard/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://api.airtable.com/v0/appXK3obBAJyNBrhI/User?api_key=keynLGHM8Jor40KNX&sort%5B0%5D%5Bfield%5D=AvailableFunds&sort%5B0%5D%5Bdirection%5D=desc&maxRecords=15')
+    .then((resp) => resp.json())
+    .then(data => {
+       this.setState({ users: data.records });
+    }).catch(err => {
+      // Error :(
+    });
+  }
+
+
+  render() {
+    return (
+      <div class="container" style={{width:600, margin:50}}>
+        <table>
+          {this.state.users.map(item => <LeaderboardItem {...item.fields} /> )}  
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+const LeaderboardItem = ({Username, AvailableFunds}) => (
+  <tr>
+    <td>{Username}</td>
+    <td>{AvailableFunds.toLocaleString()}</td>
+  </tr>
+);
---
+++ leaderboard/src/Question.js
@@ -0,0 +1,13 @@
+const Question = ({Question, Answer, CTA}) => (
+    <div class="row">
+        <div class="col-sm">
+        {Question}
+        </div>
+        <div class="col-sm">
+        {Answer}
+        </div>
+        <div class="col-sm">
+        {CTA}
+        </div>
+    </div>
+  );
