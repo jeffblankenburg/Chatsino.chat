cd2d93ce305cd118f7ac61e5d379cd120908c9b6
Jeff Blankenburg <github@jeffblankenburg.com>
2020-09-22 15:36:12 -0400

    Added leaderboard?

---
+++ leaderboard_src/build/precache-manifest.ab0e71db8fe5b74a297e8d18ba08c356.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ffacb6621c799c4283f1f87e4b583e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac3ec7b5dff313f732db",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "39f41b78c54e360f9fad",
+    "url": "/static/js/2.306083c2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.306083c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac3ec7b5dff313f732db",
+    "url": "/static/js/main.b0735dc5.chunk.js"
+  },
+  {
+    "revision": "5c3bc075e9ea892882a6",
+    "url": "/static/js/runtime-main.f67b607f.js"
+  }
+]);
